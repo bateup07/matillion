f90359e8b7c1ce5f99dd051cfbf172205077b319
Matthew Bateup <matthewbateup@253.sub-97-1-1.myvzw.com>
2017-12-19 15:21:52 +0000

    PreparedStatement added for complex SQL query

--- src/main/java/com/matillion/project/SecondPart.java
+++ src/main/java/com/matillion/project/SecondPart.java
@@ -4,10 +4,14 @@ import java.sql.*;
 
 public class SecondPart {
 
+    private static final String DEPT_DESC = "Temp Stockers";
+    private static final String PAY_TYPE = "Hourly";
+    private static final String EDUCATION_LEVEL = "Graduate Degree";
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         Connection conn;
-        Statement st;
+        PreparedStatement pst;
         ResultSet rs;
 
         String url = "jdbc:mysql://localhost:3306/foodmart";
@@ -16,14 +20,32 @@ public class SecondPart {
 
         try {
             conn = DriverManager.getConnection(url, user, password);
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from employee");
+
+            String sql = "SELECT full_name, birth_date, hire_date, gender ";
+            sql += "FROM employee ";
+            sql += "WHERE department_id = (SELECT d.department_id FROM department d WHERE d.department_description = ?) ";
+            sql += "AND position_id IN (SELECT position_id FROM position where pay_type = ?) ";
+            sql += "AND education_level = ? ";
+            sql += "ORDER BY full_name ASC;";
+
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, DEPT_DESC);
+            pst.setString(2, PAY_TYPE);
+            pst.setString(3, EDUCATION_LEVEL);
+
+            rs = pst.executeQuery();
 
             while (rs.next()) {
-                System.out.println(rs.getString(2));
+                String full_name = rs.getString("full_name");
+                String birth_date = rs.getString("birth_date");
+                String hire_date = rs.getString("hire_date");
+                String gender = rs.getString("gender");
+                System.out.println(full_name + " " + birth_date + " " + hire_date + " " + gender);
             }
+
         } catch (SQLException ex) {
             //Error message goes here
+            ex.printStackTrace();
         }
     }
 }
