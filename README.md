40c2a53a17048b446b85d6222baa9767eb403298
Matthew Bateup <matthewbateup@253.sub-97-1-1.myvzw.com>
2017-12-19 10:55:21 +0000

    General commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # matillion
+
+Areas of improvement
+==================================
+
+1/ Exception handling
+2/ Test Driven Development (TDD)
---
+++ src/main/java/com/matillion/project/SecondPart.java
@@ -0,0 +1,9 @@
+package com.matillion.project;
+
+public class SecondPart {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+}
