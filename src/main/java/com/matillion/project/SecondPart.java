abc96bafcef4ae57209b67a54e1e6fed5f1c3c58
Matthew Bateup <matthewbateup@253.sub-97-1-1.myvzw.com>
2017-12-19 14:00:12 +0000

    Basic SQL connection

--- src/main/java/com/matillion/project/SecondPart.java
+++ src/main/java/com/matillion/project/SecondPart.java
@@ -1,9 +1,29 @@
 package com.matillion.project;
 
+import java.sql.*;
+
 public class SecondPart {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        System.out.println("Hello");
+        Connection conn;
+        Statement st;
+        ResultSet rs;
+
+        String url = "jdbc:mysql://localhost:3306/foodmart";
+        String user = "root";
+        String password = "Ella150815";
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from employee");
+
+            while (rs.next()) {
+                System.out.println(rs.getString(2));
+            }
+        } catch (SQLException ex) {
+
+        }
     }
 }
