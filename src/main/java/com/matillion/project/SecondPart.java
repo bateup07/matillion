c1525313764973109d366f49b3f184da7757bac2
Matthew Bateup <matthewbateup@253.sub-97-1-1.myvzw.com>
2017-12-20 14:52:26 +0000

    Improvised Dept Description method

--- src/main/java/com/matillion/project/SecondPart.java
+++ src/main/java/com/matillion/project/SecondPart.java
@@ -1,13 +1,10 @@
 package com.matillion.project;
 
 import java.sql.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SecondPart {
-    //Redundant due to Java Menu being implemented
-    //private static final String DEPT_DESC = "Temp Stockers";
-    //private static final String PAY_TYPE = "Hourly";
-    //private static final String EDUCATION_LEVEL = "Graduate Degree";
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
@@ -38,19 +35,14 @@ public class SecondPart {
             System.out.println("Enter your department description:");
             String dept = firstOption.nextLine();
 
-            while(!dept.equalsIgnoreCase("HQ General Management") &&
-                    !dept.equalsIgnoreCase("HQ Information Systems") &&
-                    !dept.equalsIgnoreCase("HQ Marketing") &&
-                    !dept.equalsIgnoreCase("HQ Human Resources") &&
-                    !dept.equalsIgnoreCase("HQ Finance and Accounting") &&
-                    !dept.equalsIgnoreCase("Store Management") &&
-                    !dept.equalsIgnoreCase("Store Information Systems") &&
-                    !dept.equalsIgnoreCase("Permanent Checkers") &&
-                    !dept.equalsIgnoreCase("Temp Checkers") &&
-                    !dept.equalsIgnoreCase("Permanent Stockers") &&
-                    !dept.equalsIgnoreCase("Temp Stockers") &&
-                    !dept.equalsIgnoreCase("Store Permanent Butchers")) {
-                System.out.println("Please re-enter department description");
+            //load department values
+            String[] deptValuesS = new String[] {"HQ General Management","HQ Information Systems","HQ Marketing",
+                    "HQ Human Resources","HQ Finance and Accounting","Store Management",
+                    "Store Information Systems","Permanent Checkers","Temp Checkers",
+                    "Permanent Stockers","Temp Stockers","Store Permanent Butchers"};
+
+            while (!Arrays.asList(deptValuesS).contains(dept)) {
+                System.out.println("Please re-enter your department description");
                 dept = scanner.nextLine();
             }
 
@@ -59,7 +51,8 @@ public class SecondPart {
             System.out.println("Enter your pay type:");
             String pay_type = secondOption.nextLine();
 
-            while (!pay_type.equalsIgnoreCase("Hourly") && !pay_type.equalsIgnoreCase("Monthly")) {
+            while (!pay_type.equalsIgnoreCase("Hourly") &&
+                    !pay_type.equalsIgnoreCase("Monthly")) {
                 System.out.println("Please choose either Hourly or Monthly");
                 pay_type = scanner.nextLine();
             }
@@ -77,6 +70,7 @@ public class SecondPart {
                 System.out.println("Please re-enter education level");
                 education_level = scanner.nextLine();
             }
+
             pst.setString(1, dept);
             pst.setString(2, pay_type);
             pst.setString(3, education_level);
